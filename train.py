fcf203554d0f357fa2160a9625b1054eaec8ee81
Zayatsoff <rozin.lior@gmail.com>
2022-02-15 21:11:41 -0500

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+from torchvision import datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
@@ -19,3 +20,23 @@ CHANNELS_IMG = 3
 EPOCHS = 90
 MOMENTUM = 0.9
 WEIGHT_DECAY = 5e-4
+DOWNLOAD_DATASET = True
+
+# Dataset
+transformation = transforms.Compose(
+    [
+        transforms.Resize(IMAGE_SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            [0.5 for _ in range(CHANNELS_IMG)], [0.5 for _ in range(CHANNELS_IMG)]
+        ),
+    ]
+)
+
+training_data = datasets.CIFAR10(
+    root="data", train=True, download=DOWNLOAD_DATASET, transform=transformation
+)
+
+test_data = datasets.CIFAR10(
+    root="data", train=False, download=DOWNLOAD_DATASET, transform=transformation
+)
