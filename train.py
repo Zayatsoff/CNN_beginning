8a745d27312fc05f50470312e4d6d9158e245772
Zayatsoff <rozin.lior@gmail.com>
2022-02-15 21:32:20 -0500

    Update train.py

--- train.py
+++ train.py
@@ -13,13 +13,13 @@ from utils import load_checkpoint, save_checkpoint
 # HyperParams
 LOAD_MODEL = False
 device = torch.device("cuda" if torch.cuda.is_available else "cpu")
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.001  # Paper sued 0.01
 BATCH_SIZE = 128
 IMAGE_SIZE = 227
 CHANNELS_IMG = 3
 EPOCHS = 90
-MOMENTUM = 0.9
-WEIGHT_DECAY = 5e-4
+# MOMENTUM = 0.9 Used by paper
+# WEIGHT_DECAY = 5e-4 Used by paper
 DOWNLOAD_DATASET = True
 
 # Dataset
@@ -48,3 +48,22 @@ test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
 # Initializing model
 
 model = AlexNet()
+
+# Optim
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)  # Paper used SGD
+
+# Multiply lr by 1 / 10 every 30 epochs
+lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+
+
+# Training
+model.train()
+
+if LOAD_MODEL:
+    load_checkpoint("CHECKPOINT.pt", model, optimizer, LEARNING_RATE)
+
+total_steps = 1
+for epoch in range(EPOCHS):
+    lr_scheduler.step()
+    for images, classes in train_dataloader:
+        images, classes = images.to(device), classes.to(device)
