1fddd1640a96c172e7eb2a431bbdd299b71fcca2
Zayatsoff <rozin.lior@gmail.com>
2022-02-16 21:39:18 -0500

    Cleaned train.py

--- train.py
+++ train.py
@@ -1,14 +1,11 @@
 import torch
-import torch.nn as nn
 import torch.optim as optim
-import torchvision
 from torchvision import datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from model import AlexNet
 import torchvision.datasets as datasets
-from utils import load_checkpoint, save_checkpoint, train_classifier
-import torch.nn.functional as F
+from utils import load_checkpoint, train_classifier
 
 
 # HyperParams
